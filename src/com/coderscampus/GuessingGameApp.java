5660b09a1e7f0db628b66eaed92d0935ef83c631
Mark Anthony Alejo <mark.anthony552@yahoo.com>
2025-02-04 09:53:05 -0800

    GuessingGameApp
    Cleaned up the code.

--- src/com/coderscampus/GuessingGameApp.java
+++ src/com/coderscampus/GuessingGameApp.java
@@ -2,47 +2,40 @@ package com.coderscampus;
 
 import java.util.Scanner;
 
-public class Main {
+public class GuessingGameApp {
     public static void main(String[] args) {
 
-
         int numbersOfAttempts = 0;
-        int maxAttempts = 5;
+        final int MAX_ATTEMPTS = 5;
         int randomNumber = (int) (Math.random() * 100) + 1;
-        System.out.println("Pick a number between 1 and 100");
         Scanner scanner = new Scanner(System.in);
-        String guessedString = scanner.nextLine();
-        int guessedNumber = Integer.parseInt(guessedString);
 
-        while(numbersOfAttempts < maxAttempts) {
+        System.out.println("Pick a number between 1 and 100");
+
+        while(numbersOfAttempts < MAX_ATTEMPTS) {
+            int guessedNumber = Integer.parseInt(scanner.nextLine());
 
             if (guessedNumber < 1 || guessedNumber > 100) {
                 System.out.println("Your guess is not between 1 and 100, please try again");
-            } else {
+                continue;
+            }
+                numbersOfAttempts++;
 
                 if (guessedNumber == randomNumber) {
                     System.out.println("You win");
-                    break;
+                    scanner.close();
+                    return;
                 } else if (guessedNumber < randomNumber) {
                     System.out.println("Please pick a higher number");
-                    numbersOfAttempts++;
                 } else {
                     System.out.println("Please pick a lower number");
-                    numbersOfAttempts++;
                 }
 
-            }
-
-            if(numbersOfAttempts == maxAttempts){
-                System.out.println("You lose");
-                System.out.println("The number to guess was: " + randomNumber);
-                break;
-            }
-
-            guessedString = scanner.nextLine();
-            guessedNumber = Integer.parseInt(guessedString);
-
         }
+
+        System.out.println("You lose");
+        System.out.println("The number to guess was: " + randomNumber);
+        scanner.close();
 
     }
 }
